9cdbc1460c3ee170edd34f52de87566aa6ed5edb
ChemLez <liz_1106@163.com>
2019-08-24 14:18:38 +0800

    Site updated: 2019-08-24 14:18:38

--- photos/ins.js
+++ photos/ins.js
@@ -120,8 +120,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/yansheng836/yansheng836.github.io/blog/photos-source/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/yansheng836/yansheng836.github.io/blog/photos-source/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/ChemLez/blog-Picture/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/ChemLez/blog-Picture/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
